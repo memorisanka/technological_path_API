736a732969ca4d6090c5050fe368d65141b110ed
memorisanka <j.krawczyk87@gmail.com>
2023-03-18 17:58:00 +0100

    feat: add pydantic schemas (validation of incoming data)

---
+++ api/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+
+class Point(BaseModel):
+    type: str
+    lat: float
+    lon: float
+
+
+class Path(BaseModel):
+    id: str
+    name: str
+    heading: float
+    archived: bool
+    point_a: Point = Field(alias="aPoint")
+    point_b: Point = Field(alias="bPoint")
+    last_modified: str = Field(alias="lastModifiedTime")
